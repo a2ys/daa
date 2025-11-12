62e2c39742b1bed83dcdc8d4bf0e5a87c1ce7dca
a2ys <oreus.aks@gmail.com>
2025-11-12 11:34:15 +0530

    chore[standardization]: use standard orientation function

--- Geometry/ConvexHull.py
+++ Geometry/ConvexHull.py
@@ -1,3 +1,11 @@
+def orientation(o, a, b):
+    value = (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
+    if value == 0:
+        return 0
+
+    return 1 if value > 0 else 2
+
 def ConvexHull(points):
     points = sorted(points, key=lambda p: (p[1], p[0]))
     p0 = points[0]
@@ -8,14 +16,10 @@ def ConvexHull(points):
 
     points = sorted(points, key=angle)
 
-    def cross(o, a, b):
-        return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
-
     hull = []
     for p in points:
-        while len(hull) >= 2 and cross(hull[-2], hull[-1], p) <= 0:
+        while len(hull) >= 2 and orientation(hull[-2], hull[-1], p) != 1:
             hull.pop()
-
         hull.append(p)
 
     return hull
